ec490d66ff709443f1f696d69bb8bf3f6c728aec
syh369 <shenyuanhao1999@gmail.com>
2025-07-23 17:09:24 +0800

    Added rates.js; Changed routes file name to currency.js.

--- routes/currency.js
+++ routes/currency.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* PUT home page. */
+router.put('/:code/:date', putRate);
+
 export default router;
+

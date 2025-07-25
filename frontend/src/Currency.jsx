89a965c17538f2640a1e2a6b4e1772ad4c8438b8
Yamei Song <ymsongx@163.com>
2025-07-25 15:29:58 +0800

    update api

--- frontend/src/Currency.jsx
+++ frontend/src/Currency.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import api from './services/api'
 
 const Currency = () => {
+  const [exchangeRate, setExchangeRate] = useState(0);
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      try {
+        const rate = await api.getExchangeRate(1, 4, 20250724);
+        setExchangeRate(rate);
+      } catch (error) {
+        console.error('Error fetching exchange rate:', error);
+      }
+    };
+    fetchExchangeRate();
+  }, []);
+
   return (
     <div>Currency</div>
   )
